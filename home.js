842bd71f2ed91364b8069770158c507d85beb3b6
FalguniSaini <falgunisaini675@gmail.com>
2022-01-20 20:00:54 +0530

    Homepage

--- home.js
+++ home.js
@@ -38,3 +38,35 @@ function changeMode(){
     homeBody.classList.toggle("changeDark");
 }
 
+let Geolocation=()=>{
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("GeoLocation not supported.")
+    }
+
+}
+
+let showPosition=(data)=>{
+    let x = document.getElementById('weather');
+    let y = document.getElementById('degreeCel');
+    let z =  document.getElementById('weatherIcon');
+    let lon=data.coords.longitude;
+    let lat=data.coords.latitude;
+    console.log(data);
+    let url=`https://api.openweathermap.org/data/2.5/forecast/daily?lat=${lat}&lon=${lon}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`
+   
+    fetch(url)
+    .then((res)=>res.json())
+    .then((data)=>{
+        console.log(data);
+        data.list.map((item)=>{
+            console.log(item.temp.day)
+            x.innerText=`${item.temp.day} `;
+            y.innerHTML=`<img src="images/degreeCel.png" alt="degreeCel" style="height:22px; margin-top:-49%"/>`
+            z.innerHTML=`<img src='https://openweathermap.org/img/w/${item.weather[0].icon}.png' alt='weather' style="margin-left:-22%"/>`
+        })
+        
+    })
+}
+Geolocation();
